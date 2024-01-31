a7df432735b5c3c3846ea2eaef8bdc9da063f1eb
Your Name <you@example.com>
2024-01-31 02:04:46 +0000

    240131-11

--- main.tf
+++ main.tf
@@ -4,8 +4,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami		= "ami-0f3a440bbcff3d043"
   instance_type = "t3.micro"
-  
+  vpc_security_group_ids = [aws_security_group.instance.id]  
+  user_data = <<-EOF
+	#!/bin/bash
+	echo "Hello World" > index.html
+	nohup busybox httpd -f -p 8080 &
+	EOF
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+	from_port = 8080
+	to_port   = 8080
+	protocol  = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
   }
 }
